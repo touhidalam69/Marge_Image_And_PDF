82157ca723ce8ad69bfed86c58e74b5d8adbdce5
Md. Touhid Alam <touhidalam69@gmail.com>
2021-01-14 17:00:44 +0600

    Project_Upload
    Project_Upload

---
+++ imageOver/Program.cs
@@ -0,0 +1,28 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+
+namespace imageOver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyProgramPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location) + "\\files\\";
+
+            using (Stream inputPdfStream = new FileStream(MyProgramPath + "inputPdf.pdf", FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (Stream inputImageStream = new FileStream(MyProgramPath + "inputImage.png", FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (Stream outputPdfStream = new FileStream(MyProgramPath + System.DateTime.Now.ToString("yyyyMMddHHmmss") + ".pdf", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                var reader = new PdfReader(inputPdfStream);
+                var stamper = new PdfStamper(reader, outputPdfStream);
+                var pdfContentByte = stamper.GetOverContent(1);
+
+                Image image = Image.GetInstance(inputImageStream);
+                image.SetAbsolutePosition(100, 100);
+                pdfContentByte.AddImage(image);
+                stamper.Close();
+            }
+        }
+    }
+}
